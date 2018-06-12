fa0e2082495cc36424663e91f10aa180081c4b51
heyu <784242998@qq.com>
2018-06-12 15:57:45 +0800

    product

--- src/components/App/Products/EcsSnapshot.vue
+++ src/components/App/Products/EcsSnapshot.vue
@@ -179,20 +179,19 @@
 
 <style rel="stylesheet/less" lang="less" scoped>
   .logo {
-    height: 10rem;
     background: #081633;
     .logo-wrapper {
       display: flex;
-      padding: 1rem .8rem;
+      padding: .5rem .3rem;
       > div {
         width: 65%;
         .title {
           color: rgba(255, 255, 255, 1);
-          font-size: .7rem;
-          padding-bottom: .5rem;
+          font-size: .24rem;
+          padding-bottom: .15rem;
         }
         .desc {
-          font-size: .6rem;
+          font-size: .2rem;
           color: rgba(255, 255, 255, 1);
         }
       }
@@ -202,29 +201,28 @@
 
   .features {
     background: rgba(243, 243, 243, 1);
-    margin-bottom: .5rem;
     .features-header {
       text-align: center;
-      font-size: 1.5rem;
-      color: rgba(74, 144, 226, 1);
       border-bottom: 1px solid #D9D9D9;
+      padding: .43rem 0;
       h6 {
-        line-height: 3rem;
+        font-size: .36rem;
+        color: rgba(74, 144, 226, 1);
         &::before {
           content: '';
-          width: .5rem;
-          height: .5rem;
+          width: .2rem;
+          height: .2rem;
           border: 1px solid #4A90E2;
           display: inline-block;
-          margin-right: .5rem;
+          margin-right: .2rem;
         }
         &::after {
           content: '';
-          width: .5rem;
-          height: .5rem;
+          width: .2rem;
+          height: .2rem;
           border: 1px solid #4A90E2;
           display: inline-block;
-          margin-left: .5rem;
+          margin-left: .2rem;
         }
       }
     }
@@ -232,20 +230,21 @@
       background: rgba(255, 255, 255, 1);
       .item {
         display: flex;
-        padding: .5rem .8rem;
+        padding: .3rem;
         border-bottom: 1px solid #D9D9D9;
         img {
-          margin-right: .5rem;
+          width: .8rem;
+          height: .8rem;
+          margin-right: .25rem;
         }
         .features-title {
-          font-size: .8rem;
+          font-size: .32rem;
           color: rgba(34, 34, 34, 1);
-          margin-bottom: .5rem;
+          margin-bottom: .16rem;
         }
         .features-desc {
-          font-size: .6rem;
+          font-size: .2rem;
           color: rgba(153, 153, 153, 1);
-          line-height: 1rem;
           word-wrap: break-word;
         }
       }
@@ -254,71 +253,64 @@
 
   .stageInfo {
     background: rgba(243, 243, 243, 1);
-    margin-bottom: .5rem;
     .stageInfo-header {
       text-align: center;
-      font-size: 1.5rem;
-      color: rgba(74, 144, 226, 1);
       border-bottom: 1px solid #D9D9D9;
+      padding: .43rem 0;
       h6 {
-        line-height: 3rem;
+        font-size: .36rem;
+        color: rgba(74, 144, 226, 1);
         &::before {
           content: '';
-          width: .5rem;
-          height: .5rem;
+          width: .2rem;
+          height: .2rem;
           border: 1px solid #4A90E2;
           display: inline-block;
-          margin-right: .5rem;
+          margin-right: .2rem;
         }
         &::after {
           content: '';
-          width: .5rem;
-          height: .5rem;
+          width: .2rem;
+          height: .2rem;
           border: 1px solid #4A90E2;
           display: inline-block;
-          margin-left: .5rem;
+          margin-left: .2rem;
         }
       }
     }
     .stageInfo-content {
       background: rgba(255, 255, 255, 1);
       .tab-item {
-        font-size: .8rem;
+        font-size: .32rem;
         color: #333;
       }
       .stageInfo-item {
         background: rgba(250, 250, 250, 1);
-        padding: .5rem .8rem;
+        padding: .5rem .3rem;
         .describe {
           h6 {
-            font-size: .8rem;
+            font-size: .32rem;
             color: #333;
-            padding: .5rem 0;
           }
           ul {
-            padding: .5rem 0;
-            height: 5rem;
+            height: 2rem;
             li {
               list-style: none;
-              font-size: .6rem;
+              font-size: .2rem;
               color: #999;
               &::before {
                 content: '';
-                width: .25rem;
-                height: .25rem;
+                width: .1rem;
+                height: .1rem;
                 background: #999;
                 display: inline-block;
                 border-radius: 50%;
                 position: relative;
-                bottom: .1rem;
-                margin-right: .2rem;
+                bottom: .03rem;
+                margin-right: .1rem;
               }
             }
           }
-        }
-        img {
-          width: 15rem;
-          height: 15rem;
         }
       }
     }
@@ -328,26 +320,26 @@
     background: rgba(243, 243, 243, 1);
     .advantage-header {
       text-align: center;
-      font-size: 1.5rem;
-      color: rgba(74, 144, 226, 1);
       border-bottom: 1px solid #D9D9D9;
+      padding: .43rem 0;
       h6 {
-        line-height: 3rem;
+        font-size: .36rem;
+        color: rgba(74, 144, 226, 1);
         &::before {
           content: '';
-          width: .5rem;
-          height: .5rem;
+          width: .2rem;
+          height: .2rem;
           border: 1px solid #4A90E2;
           display: inline-block;
-          margin-right: .5rem;
+          margin-right: .2rem;
         }
         &::after {
           content: '';
-          width: .5rem;
-          height: .5rem;
+          width: .2rem;
+          height: .2rem;
           border: 1px solid #4A90E2;
           display: inline-block;
-          margin-left: .5rem;
+          margin-left: .2rem;
         }
       }
     }
@@ -355,37 +347,38 @@
       background: rgba(255, 255, 255, 1);
       .item {
         display: flex;
-        padding: .5rem .8rem;
+        padding: .3rem;
         border-bottom: 1px solid #D9D9D9;
         img {
-          margin-right: .5rem;
+          width: .8rem;
+          height: .8rem;
+          margin-right: .25rem;
         }
         .advantage-title {
-          font-size: .8rem;
+          font-size: .32rem;
           color: rgba(34, 34, 34, 1);
-          margin-bottom: .5rem;
+          margin-bottom: .16rem;
         }
         .advantage-desc {
-          font-size: .6rem;
+          font-size: .2rem;
           color: rgba(153, 153, 153, 1);
-          line-height: 1rem;
           word-wrap: break-word;
         }
       }
     }
     .advantage-bottom {
-      height: 3rem;
+      height: .69rem;
     }
   }
 
   button {
-    font-size: .8rem;
+    font-size: .32rem;
     color: #FFF;
     background: #4A90E2;
     border: none;
     outline: none;
     width: 100%;
-    padding: .9rem 0;
+    padding: .32rem 0;
     text-align: center;
   }
 </style>
--- src/components/App/Products/Virtualvpn.vue
+++ src/components/App/Products/Virtualvpn.vue
@@ -37,21 +37,22 @@
       <div class="stageInfo-content">
         <tab :line-width="2" active-color="#4A90E2">
           <tab-item selected @on-item-click="stageInfo.showYY=true,stageInfo.showSJ=false">
-            <h6 class="tab-item">云服务器</h6>
+            <h6 class="tab-item">混合云部署</h6>
           </tab-item>
           <tab-item @on-item-click="stageInfo.showYY=false,stageInfo.showSJ=true">
-            <h6 class="tab-item">虚拟私有云VPC</h6>
+            <h6 class="tab-item">跨地域VPN互联</h6>
           </tab-item>
         </tab>
         <div class="stageInfo-item">
-          <img src="">
           <div class="describe" v-if="stageInfo.showYY">
+            <img src="../../../assets/img/vpn/stageInfo-bushu.png">
             <h6>场景描述</h6>
             <ul>
               <li v-for="(item,index) in stageInfo.YY.text" :key="index">{{item}}</li>
             </ul>
           </div>
           <div class="describe" v-if="stageInfo.showSJ">
+            <img src="../../../assets/img/vpn/stageInfo-vpn.png">
             <h6>场景描述</h6>
             <ul>
               <li v-for="(item,index) in stageInfo.SJ.text" :key="index">{{item}}</li>
@@ -159,20 +160,19 @@
 
 <style rel="stylesheet/less" lang="less" scoped>
   .logo {
-    height: 10rem;
     background: #081633;
     .logo-wrapper {
       display: flex;
-      padding: 1rem .8rem;
+      padding: .5rem .3rem;
       > div {
         width: 65%;
         .title {
           color: rgba(255, 255, 255, 1);
-          font-size: .7rem;
-          padding-bottom: .5rem;
+          font-size: .24rem;
+          padding-bottom: .15rem;
         }
         .desc {
-          font-size: .6rem;
+          font-size: .2rem;
           color: rgba(255, 255, 255, 1);
         }
       }
@@ -182,29 +182,28 @@
 
   .features {
     background: rgba(243, 243, 243, 1);
-    margin-bottom: .5rem;
     .features-header {
       text-align: center;
-      font-size: 1.5rem;
-      color: rgba(74, 144, 226, 1);
       border-bottom: 1px solid #D9D9D9;
+      padding: .43rem 0;
       h6 {
-        line-height: 3rem;
+        font-size: .36rem;
+        color: rgba(74, 144, 226, 1);
         &::before {
           content: '';
-          width: .5rem;
-          height: .5rem;
+          width: .2rem;
+          height: .2rem;
           border: 1px solid #4A90E2;
           display: inline-block;
-          margin-right: .5rem;
+          margin-right: .2rem;
         }
         &::after {
           content: '';
-          width: .5rem;
-          height: .5rem;
+          width: .2rem;
+          height: .2rem;
           border: 1px solid #4A90E2;
           display: inline-block;
-          margin-left: .5rem;
+          margin-left: .2rem;
         }
       }
     }
@@ -212,20 +211,21 @@
       background: rgba(255, 255, 255, 1);
       .item {
         display: flex;
-        padding: .5rem .8rem;
+        padding: .3rem;
         border-bottom: 1px solid #D9D9D9;
         img {
-          margin-right: .5rem;
+          width: .8rem;
+          height: .8rem;
+          margin-right: .25rem;
         }
         .features-title {
-          font-size: .8rem;
+          font-size: .32rem;
           color: rgba(34, 34, 34, 1);
-          margin-bottom: .5rem;
+          margin-bottom: .16rem;
         }
         .features-desc {
-          font-size: .6rem;
+          font-size: .2rem;
           color: rgba(153, 153, 153, 1);
-          line-height: 1rem;
           word-wrap: break-word;
         }
       }
@@ -234,71 +234,70 @@
 
   .stageInfo {
     background: rgba(243, 243, 243, 1);
-    margin-bottom: .5rem;
     .stageInfo-header {
       text-align: center;
-      font-size: 1.5rem;
-      color: rgba(74, 144, 226, 1);
       border-bottom: 1px solid #D9D9D9;
+      padding: .43rem 0;
       h6 {
-        line-height: 3rem;
+        font-size: .36rem;
+        color: rgba(74, 144, 226, 1);
         &::before {
           content: '';
-          width: .5rem;
-          height: .5rem;
+          width: .2rem;
+          height: .2rem;
           border: 1px solid #4A90E2;
           display: inline-block;
-          margin-right: .5rem;
+          margin-right: .2rem;
         }
         &::after {
           content: '';
-          width: .5rem;
-          height: .5rem;
+          width: .2rem;
+          height: .2rem;
           border: 1px solid #4A90E2;
           display: inline-block;
-          margin-left: .5rem;
+          margin-left: .2rem;
         }
       }
     }
     .stageInfo-content {
       background: rgba(255, 255, 255, 1);
       .tab-item {
-        font-size: .8rem;
+        font-size: .32rem;
         color: #333;
       }
       .stageInfo-item {
         background: rgba(250, 250, 250, 1);
-        padding: .5rem .8rem;
+        padding: .5rem .3rem;
         .describe {
+          img{
+            width: 4.71rem;
+            display: block;
+            margin: 0 auto;
+            padding-bottom: .3rem;
+          }
           h6 {
-            font-size: .8rem;
+            font-size: .32rem;
             color: #333;
-            padding: .5rem 0;
           }
           ul {
-            padding: .5rem 0;
-            height: 5rem;
+            height: 2.5rem;
             li {
               list-style: none;
-              font-size: .6rem;
+              font-size: .2rem;
               color: #999;
               &::before {
                 content: '';
-                width: .25rem;
-                height: .25rem;
+                width: .1rem;
+                height: .1rem;
                 background: #999;
                 display: inline-block;
                 border-radius: 50%;
                 position: relative;
-                bottom: .1rem;
-                margin-right: .2rem;
+                bottom: .03rem;
+                margin-right: .1rem;
               }
             }
           }
-        }
-        img {
-          width: 15rem;
-          height: 15rem;
         }
       }
     }
@@ -308,26 +307,26 @@
     background: rgba(243, 243, 243, 1);
     .advantage-header {
       text-align: center;
-      font-size: 1.5rem;
-      color: rgba(74, 144, 226, 1);
       border-bottom: 1px solid #D9D9D9;
+      padding: .43rem 0;
       h6 {
-        line-height: 3rem;
+        font-size: .36rem;
+        color: rgba(74, 144, 226, 1);
         &::before {
           content: '';
-          width: .5rem;
-          height: .5rem;
+          width: .2rem;
+          height: .2rem;
           border: 1px solid #4A90E2;
           display: inline-block;
-          margin-right: .5rem;
+          margin-right: .2rem;
         }
         &::after {
           content: '';
-          width: .5rem;
-          height: .5rem;
+          width: .2rem;
+          height: .2rem;
           border: 1px solid #4A90E2;
           display: inline-block;
-          margin-left: .5rem;
+          margin-left: .2rem;
         }
       }
     }
@@ -335,37 +334,38 @@
       background: rgba(255, 255, 255, 1);
       .item {
         display: flex;
-        padding: .5rem .8rem;
+        padding: .3rem;
         border-bottom: 1px solid #D9D9D9;
         img {
-          margin-right: .5rem;
+          width: .8rem;
+          height: .8rem;
+          margin-right: .25rem;
         }
         .advantage-title {
-          font-size: .8rem;
+          font-size: .32rem;
           color: rgba(34, 34, 34, 1);
-          margin-bottom: .5rem;
+          margin-bottom: .16rem;
         }
         .advantage-desc {
-          font-size: .6rem;
+          font-size: .2rem;
           color: rgba(153, 153, 153, 1);
-          line-height: 1rem;
           word-wrap: break-word;
         }
       }
     }
     .advantage-bottom {
-      height: 3rem;
+      height: .69rem;
     }
   }
 
   button {
-    font-size: .8rem;
+    font-size: .32rem;
     color: #FFF;
     background: #4A90E2;
     border: none;
     outline: none;
     width: 100%;
-    padding: .9rem 0;
+    padding: .32rem 0;
     text-align: center;
   }
 </style>
