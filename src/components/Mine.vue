e8eeb97a06ea5adb54b42298458534df19043851
heyu <784242998@qq.com>
2018-06-14 11:56:30 +0800

    mine

--- src/components/Mine.vue
+++ src/components/Mine.vue
@@ -22,23 +22,96 @@
       </div>
     </div>
     <div class="group">
-      <group v-for="(cellList,index) in groupList" :key="index">
-        <cell-box v-for="(cell,i) in cellList" :key="i" is-link :link="cell.url">
-            <div class="cell" style="position: relative">
-            <img :src="cell.img">
-            <div>
-              <p style="font-size: .24rem"><label>{{cell.title}}</label></p>
-            </div>
+      <!--<group v-for="(cellList,index) in groupList" :key="index">-->
+        <!--<cell-box v-for="(cell,i) in cellList" :key="i" is-link :link="cell.url">-->
+            <!--<div class="cell">-->
+            <!--<img :src="cell.img">-->
+            <!--<div>-->
+              <!--<p style="font-size: .24rem;padding-right: 3rem"><label>{{cell.title}}</label></p>-->
+            <!--</div>-->
+              <!--<badge text="15" v-if="cell.title=='消息中心'"></badge>-->
+          <!--</div>-->
+        <!--</cell-box>-->
+      <!--</group>-->
+
+      <!--版块一-->
+      <div>
+        <div class="r-item">
+          <div>
+            <img src="../assets/img/mine/rezheng.png">
+            <p>实名认证</p>
           </div>
-          <span style="position: absolute;right: 1rem" v-if="cell.title=='消息中心'"><badge text="15"></badge></span>
-        </cell-box>
-      </group>
+        </div>
+        <div class="s-item">
+          <div>
+            <img src="../assets/img/mine/shezhi.png">
+            <p>安全设置</p>
+          </div>
+        </div>
+      </div>
+      <!--版块二-->
+      <div>
+        <div class="dd-item">
+          <div>
+            <img src="../assets/img/mine/dingdan.png">
+            <p>订单管理</p>
+          </div>
+        </div>
+        <div class="xf-item">
+          <div>
+            <img src="../assets/img/mine/xiaofei.png">
+            <p>当月消费</p>
+          </div>
+        </div>
+        <div class="fp-item">
+          <div>
+            <img src="../assets/img/mine/fapiao.png">
+            <p>发票管理</p>
+          </div>
+        </div>
+      </div>
+      <!--版块三-->
+      <div>
+        <div class="xx-item" @click="$router.push('newscenter')">
+          <div>
+            <img src="../assets/img/mine/xiaoxi.png">
+            <p>消息中心</p>
+           <badge text="15"></badge>
+          </div>
+        </div>
+      </div>
+      <!--版块四-->
+      <div>
+        <div class="r-item">
+          <div>
+            <img src="../assets/img/mine/yijian.png">
+            <p>意见反馈</p>
+          </div>
+        </div>
+        <div class="s-item" @click="showHF=true">
+          <div>
+            <img src="../assets/img/mine/dianhua.png">
+            <p>客服电话</p>
+          </div>
+        </div>
+      </div>
+      <!--版块五-->
+      <div>
+        <div class="xx-item">
+          <div>
+            <img src="../assets/img/mine/tuichu.png">
+            <p>安全退出</p>
+          </div>
+        </div>
+      </div>
     </div>
+
+    <actionsheet v-model="showHF" :menus="menusKF" show-cancel></actionsheet>
   </div>
 </template>
 
 <script>
-  import {Group, CellBox,Badge} from 'vux'
+  import {Group, CellBox,Badge,Actionsheet} from 'vux'
   import axios from '@/util/iaxios'
   import $store from '@/vuex'
   import {mapState} from 'vuex'
@@ -46,7 +119,8 @@
     components: {
       Group,
       CellBox,
-      Badge
+      Badge,
+      Actionsheet
     },
     data () {
       scrollTo( 0, 0 )
@@ -55,25 +129,31 @@
         voucher: '',
         groupList: [
           [
-            {title: '实名认证', url: '/home',img:require('../assets/img/mine/rezheng.png')},
-            {title: '安全设置', url: '/home',img:require('../assets/img/mine/shezhi.png')}
+            {title: '实名认证', img:require('../assets/img/mine/rezheng.png')},
+            {title: '安全设置',img:require('../assets/img/mine/shezhi.png')}
           ],
           [
-            {title: '订单管理', url: '/home',img:require('../assets/img/mine/dingdan.png')},
-            {title: '当月消费', url: '/home',img:require('../assets/img/mine/xiaofei.png')}
+            {title: '订单管理',img:require('../assets/img/mine/dingdan.png')},
+            {title: '当月消费',img:require('../assets/img/mine/xiaofei.png')}
           ],
           [
             {title: '消息中心', url: '/newscenter',img:require('../assets/img/mine/xiaoxi.png')}
           ],
           [
-            {title: '意见反馈', url: '/home',img:require('../assets/img/mine/yijian.png')},
-            {title: '客服电话', url: '/home',img:require('../assets/img/mine/dianhua.png')}
+            {title: '意见反馈', img:require('../assets/img/mine/yijian.png')},
+            {title: '客服电话', img:require('../assets/img/mine/dianhua.png')}
           ],
           [
-            {title: '安全退出', url: '/login',img:require('../assets/img/mine/tuichu.png')}
+            {title: '安全退出', img:require('../assets/img/mine/tuichu.png')}
           ],
         ],
-        newsCounts:null  // 消息个数
+        newsCounts:null,  // 消息个数
+        showHF:false,
+        menusKF: {
+          menu1: '客服电话',
+          menu2: '<span style="color:red">拨打 400-050-5565</span>'
+        },
+
       }
     },
     methods: {
@@ -124,7 +204,7 @@
             height: .11rem;
             border-left: 1px solid #fff;
             border-bottom: 1px solid #fff;
-            transform: translateY(-5%) rotate(-135deg);
+            transform: translateY(-.05rem) rotate(-135deg);
             margin-left: 4.3rem;
           }
         }
@@ -155,14 +235,145 @@
   }
 
   .group {
-    .cell {
-      display: flex;
-      align-items: center;
-      img {
-        width: .48rem;
-        height: .48rem;
-        display: block;
-        margin-right:.24rem
+         /*.cell {*/
+           /*display: flex;*/
+           /*align-items: center;*/
+           /*img {*/
+             /*width: .48rem;*/
+             /*display: block;*/
+             /*margin-right:.24rem*/
+           /*}*/
+       /*}*/
+
+
+    .r-item{
+      padding-top: .2rem;
+      background: #FFF;
+      padding-left: .3rem;
+      div{
+        display: flex;
+        align-items: center;
+        padding-bottom: .2rem;
+        border-bottom: 1px solid #D9D9D9;
+        img{
+          width: .48rem;
+          display: block;
+          margin-right: .24rem;
+        }
+        p{
+          font-size: .28rem;
+          color: #222;
+        }
+
+      }
+    }
+    .s-item{
+      padding-top: .2rem;
+      background: #FFF;
+      border-bottom: 1px solid #D9D9D9;
+      margin-bottom: .2rem;
+      div{
+        display: flex;
+        align-items: center;
+        padding-bottom: .2rem;
+        padding-left: .3rem;
+        img{
+          width: .48rem;
+          display: block;
+          margin-right: .24rem;
+        }
+        p{
+          font-size: .28rem;
+          color: #222;
+        }
+
+      }
+    }
+    .dd-item{
+      padding-top: .2rem;
+      background: #FFF;
+      padding-left: .3rem;
+      div{
+        display: flex;
+        align-items: center;
+        padding-bottom: .2rem;
+        border-bottom: 1px solid #D9D9D9;
+        img{
+          width: .48rem;
+          display: block;
+          margin-right: .24rem;
+        }
+        p{
+          font-size: .28rem;
+          color: #222;
+        }
+
+      }
+    }
+    .xf-item{
+      padding-top: .2rem;
+      background: #FFF;
+      padding-left: .3rem;
+      div{
+        display: flex;
+        align-items: center;
+        padding-bottom: .2rem;
+        border-bottom: 1px solid #D9D9D9;
+        img{
+          width: .48rem;
+          display: block;
+          margin-right: .24rem;
+        }
+        p{
+          font-size: .28rem;
+          color: #222;
+        }
+
+      }
+    }
+    .fp-item{
+      padding-top: .2rem;
+      background: #FFF;
+      border-bottom: 1px solid #D9D9D9;
+      margin-bottom: .2rem;
+      div{
+        display: flex;
+        align-items: center;
+        padding-bottom: .2rem;
+        padding-left: .3rem;
+        img{
+          width: .48rem;
+          display: block;
+          margin-right: .24rem;
+        }
+        p{
+          font-size: .28rem;
+          color: #222;
+        }
+
+      }
+    }
+    .xx-item{
+      padding-top: .2rem;
+      background: #FFF;
+      border-bottom: 1px solid #D9D9D9;
+      margin-bottom: .2rem;
+      div{
+        display: flex;
+        align-items: center;
+        padding-bottom: .2rem;
+        padding-left: .3rem;
+        img{
+          width: .48rem;
+          display: block;
+          margin-right: .24rem;
+        }
+        p{
+          font-size: .28rem;
+          color: #222;
+          padding-right: 3rem;
+        }
+
       }
     }
   }
